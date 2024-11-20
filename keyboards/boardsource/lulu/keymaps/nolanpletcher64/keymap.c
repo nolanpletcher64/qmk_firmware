92847bee5fbd7bf2431bd58e1482c53d0a9512ce
Nolan Pletcher <nolan.pletcher@us.abb.com>
2024-11-20 14:06:28 -0800

    Fixed lower layer brackets

--- keyboards/boardsource/lulu/keymaps/nolanpletcher64/keymap.c
+++ keyboards/boardsource/lulu/keymaps/nolanpletcher64/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   `  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |  -   |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |LSHIFT|      |      |      |      |      |-------|    |-------|      |      |   {  |   }  |   |  |RSHIFT|
+ * |LSHIFT|      |      |      |      |      |-------|    |-------|      |      |   [  |   ]  |   |  |RSHIFT|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LCtrl| LAlt |LOWER | /Space  /       \Enter \  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PLUS,
   KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
-  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_PIPE, KC_RSFT,
+  KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_PIPE, KC_RSFT,
                              KC_LCTL, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_BSPC, KC_RGUI
 ),
 /* RAISE
